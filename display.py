16f462337733c3a9f9788c442562dc05d97929d8
rinintachuen <burin.ina@student.mahidol.edu>
2024-07-07 11:29:38 +0700

    make display module

---
+++ display.py
@@ -0,0 +1,4 @@
+class display:
+    def __init__(self, name):
+        # initialize variables here and def functions under
+        self.name = name
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import door
 import led
 import camera
+import display
 
 
 def main():
+    #where all the main stuff goes
     print(0)
     return 0
 
