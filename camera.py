6b189b44f834753d13d0141a6dceb1f37948f7c4
rinintachuen <burin.ina@student.mahidol.edu>
2024-07-07 11:26:51 +0700

    initialize modules as flat structure

---
+++ camera.py
@@ -0,0 +1,3 @@
+class camera:
+    def __init__(self,name):
+        self.name = name
---
+++ main.py
@@ -0,0 +1,12 @@
+import door
+import led
+import camera
+
+
+def main():
+    print(0)
+    return 0
+
+
+if __name__ == "__main__":
+    main()
